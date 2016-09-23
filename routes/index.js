075ba720d7d1d0dc78164f3ef2628583ad2b2f7a
jm921106 <jm921106@naver.com>
2016-09-23 17:06:14 +0900

    dir정리

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/logicong', function(req, res, next) {
+  res.render('logicong');
+});
+
+router.get('/swEdu', function(req, res, next) {
+  res.render('swEdu');
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog');
+});
+
+router.get('/notice', function(req, res, next) {
+  res.render('notice');
+});
+
+router.get('/counsel', function(req, res, next) {
+  res.render('counsel');
+});
+
+module.exports = router;
